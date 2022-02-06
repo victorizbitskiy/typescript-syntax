e5d35e2ffb21df5cac2225e9afca028c2db3f1a1
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-02-06 23:16:12 +0300

    bug fix

--- 6_guards.ts
+++ 6_guards.ts
@@ -4,3 +4,38 @@ function strip(x: string | number) {
   }
   return x.trim()
 }
+
+class myResponse {
+  header = 'response header'
+  result = 'response result'
+}
+
+class myError {
+  header = 'error header'
+  message = 'error result'
+}
+
+function handle(res: myResponse | myError) {
+  if (res instanceof myResponse) {
+    return {
+      info: res.header + res.result
+    }
+  } else {
+    return {
+      info: res.header + res.message
+    }
+  }
+}
+
+//  ====
+
+type AllertType = 'success' | 'danger' | 'warning'
+
+function setAllertType(type: AllertType) {
+  // ..
+}
+
+setAllertType('success')
+setAllertType('warning')
+
+// setAllertType('default')
